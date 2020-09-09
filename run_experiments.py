c75d9aeba9a5d4d047df235d22c3148b0e9ab065
Victor Samayoa <vasamayoa@gmail.com>
2020-09-09 11:44:36 -0500

    Fixed several error in the variables names and import datatime.

--- run_experiments.py
+++ run_experiments.py
@@ -3,6 +3,7 @@ from train_model.model_utils import buildDataset
 from train_model.model_utils import Experiments
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.regularizers import L2
+import datetime
 
 # Defining variables
 NUMBER_CATEGORIES = 7
@@ -27,7 +28,7 @@ for cnn_model_name in cnn_models:
     train, val = dataset.create_train_dataset(model = base_model.model, preprocess_input = base_model.preprocess_input)
 
     # Training the model
-    my_experiment = Experiments(cnn_model, my_model, train, val, optimizer = Adam(0.01, beta_1 = 0.75, beta_2 = 0.85, epsilon = 1))
+    my_experiment = Experiments(cnn_model_name, base_model.model, train, val, optimizer = Adam(0.01, beta_1 = 0.75, beta_2 = 0.85, epsilon = 1))
     new_experiment = my_experiment.train_model(cnn_model_name + 'v1.0', epochs = 50, checkpoint_path = model_chkp)
 
     print("Finishing hard training for "  + cnn_model_name)
